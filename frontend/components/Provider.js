15356f87cddc6152914822631cec8dfdfec7624b
karanjot-s <karanjot2409@gmail.com>
2023-09-17 20:02:52 +0530

    converted react code to next

---
+++ frontend/components/Provider.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const Provider = ({ children }) => {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+};
+
+export default Provider;
---
+++ frontend/pages/auth/login/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>hi from login</div>;
+};
+
+export default Login;
