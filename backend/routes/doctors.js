160b2dfd205a68306cfbc7318ed8b8769c875abb
AnshGoyal27 <anshgoyal.cse@gmail.com>
2023-09-17 23:33:46 +0530

    Created routes and encryption

---
+++ backend/routes/doctors.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/login',(req,res)=>{
+
+})
+
+router.post('/signup',(req,res)=>{
+    
+})
+
+module.exports = router;
