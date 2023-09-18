60edf1e1c99394a20e0be619dbd3a323a7ca44ef
AnshGoyal27 <anshgoyal.cse@gmail.com>
2023-09-18 20:34:03 +0530

    Updated Encryption and Authorisation

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const { encrypt } = require('../utils/encrypt');
+const { assignToken } = require('../utils/jwt');
 const router = express.Router();
 
 router.post('/login',(req,res)=>{
-
+    assignToken(user,'2h').then((token)=>{
+        console.log("token",token);
+    })
 })
 
 router.post('/signup',(req,res)=>{
-    
+    const {user,password} = req.body;
+
+    encrypt(password,15).then((pass)=>{
+        console.log("Hashed: ",pass);
+    })
+
+    assignToken(user,'2h').then((token)=>{
+        console.log("token",token);
+    })
+
 })
 
 module.exports = router;
--- backend/routes/patients.js
+++ backend/routes/patients.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const { encrypt } = require('../utils/encrypt');
+const { assignToken } = require('../utils/jwt');
 const router = express.Router();
 
 router.post('/login',async (req,res)=>{
 
-    const {password} = req.body;
-
-    encrypt(password).then((pass)=>{
-        console.log("Hashed: ",pass);
+    assignToken(user,'1h').then((token)=>{
+        console.log("token",token);
     })
 
 })
 
 router.post('/signup',(req,res)=>{
+    const {user,password} = req.body;
+
+    encrypt(password,10).then((pass)=>{
+        console.log("Hashed: ",pass);
+    })
+
+    assignToken(user,'1h').then((token)=>{
+        console.log("token",token);
+    })
 
 })
 
