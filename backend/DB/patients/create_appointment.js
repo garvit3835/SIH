84bd748aa24d30c47c1305be77a0e843b2dc9da7
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2023-09-18 20:14:44 +0530

    db

---
+++ backend/DB/patients/create_appointment.js
@@ -0,0 +1,31 @@
+const pool = require("../../config/connect_db");
+
+const create_appointment = async (
+	p_id,
+	d_id,
+	h_id,
+	description,
+	time,
+	is_emergency,
+	status,
+	prescription
+) => {
+	const query =
+		"INSERT INTO appointments (patient, doctor, hospital, description, time, is_emergency, status, prescription) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+	const values = [
+		p_id,
+		d_id,
+		h_id,
+		description,
+		time,
+		is_emergency,
+		status,
+		prescription,
+	];
+	await pool.connect();
+	try {
+		await pool.query(query, values);
+	} finally {
+		await pool.end();
+	}
+};
