5ad2a23eb60d2fd158c862b007c02d0685d16609
karanjot-s <karanjot2409@gmail.com>
2023-09-19 20:11:52 +0530

    Updated Signup UI

---
+++ frontend/components/auth/SignupForm.js
@@ -0,0 +1,154 @@
+import cn from "classnames";
+
+import {
+  Center,
+  Image,
+  Input,
+  FormControl,
+  FormLabel,
+  Button,
+  Select,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Link,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+
+import {
+  AiOutlineMail,
+  AiFillEye,
+  AiFillLock,
+  AiFillEyeInvisible,
+} from "react-icons/ai";
+import styles from "pages/auth/signup/signup.module.css";
+import { useState } from "react";
+
+const SignupForm = ({ role }) => {
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const handleClick1 = () => setShow1(!show1);
+  const handleClick2 = () => setShow2(!show2);
+
+  return (
+    <Center className={cn(styles.rightMain, styles[role])}>
+      <FormControl className={styles.form}>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <AiOutlineMail className={styles.icon} />
+          </InputLeftElement>
+          <Input
+            type="email"
+            placeholder="Email"
+            isRequired
+            color={
+              role === "patient"
+                ? "teal"
+                : role === "doctor"
+                ? "purple"
+                : "orange"
+            }
+            _placeholder={{ color: "inherit" }}
+            className={styles.inp}
+          />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement>
+            <AiFillLock className={styles.icon} />
+          </InputLeftElement>
+          <Input
+            placeholder="Password"
+            isRequired
+            type={show1 ? "text" : "password"}
+            color={
+              role === "patient"
+                ? "teal"
+                : role === "doctor"
+                ? "purple"
+                : "orange"
+            }
+            _placeholder={{ color: "inherit" }}
+            className={styles.inp}
+          />
+          <InputRightElement
+            onClick={() => {
+              handleClick1();
+            }}
+          >
+            {show1 ? (
+              <AiFillEye className={styles.icon} />
+            ) : (
+              <AiFillEyeInvisible className={styles.icon} />
+            )}
+          </InputRightElement>
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement>
+            <AiFillLock className={styles.icon} />
+          </InputLeftElement>
+          <Input
+            placeholder="Confirm Password"
+            isRequired
+            type={show2 ? "text" : "password"}
+            color={
+              role === "patient"
+                ? "teal"
+                : role === "doctor"
+                ? "purple"
+                : "orange"
+            }
+            _placeholder={{ color: "inherit" }}
+            className={styles.inp}
+          />
+          <InputRightElement
+            onClick={() => {
+              handleClick2();
+            }}
+          >
+            {show2 ? (
+              <AiFillEye className={styles.icon} />
+            ) : (
+              <AiFillEyeInvisible className={styles.icon} />
+            )}
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme={
+          role === "patient" ? "teal" : role === "doctor" ? "purple" : "orange"
+        }
+        variant="outline"
+        className={styles.signupBtn}
+      >
+        Signup
+      </Button>
+      <Center className={styles.googleContainer}>
+        <p className={styles.p}>Or</p>
+
+        <button className={styles.googleBtn}>
+          <img src="/images/google-logo.png" width={"40px"}></img>
+        </button>
+        {/* <button className={styles.googleBtn}>
+      <img src="/images/facebook-logo.png" width={"40px"}></img>
+
+    </button>
+    <button className={styles.googleBtn}>
+      <img src="/images/X-logo.png" width={"40px"}></img>
+
+    </button> */}
+        <p className={styles.p}>
+          Already have an account?{" "}
+          <Link className={styles.link} href="/auth/login">
+            Click here
+          </Link>
+        </p>
+      </Center>
+    </Center>
+  );
+};
+
+export default SignupForm;
--- frontend/pages/auth/signup/index.js
+++ frontend/pages/auth/signup/index.js
@@ -1,118 +1,109 @@
 import cn from "classnames";
 import React, { useEffect, useState } from "react";
-import { Center, Image, Input, FormControl, FormLabel, Button, Select, InputGroup, InputLeftElement, InputRightElement, Link, } from "@chakra-ui/react";
-import { AiOutlineMail, AiFillEye, AiFillLock, AiFillEyeInvisible } from "react-icons/ai";
-import styles from "./signup.module.css";
-import { AnimatePresence, easeOut, motion } from "framer-motion"
+import {
+  Center,
+  Image,
+  Input,
+  FormControl,
+  FormLabel,
+  Button,
+  Select,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Link,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+
+import {
+  AiOutlineMail,
+  AiFillEye,
+  AiFillLock,
+  AiFillEyeInvisible,
+} from "react-icons/ai";
+import styles from "pages/auth/signup/signup.module.css";
+import { AnimatePresence, easeOut, motion } from "framer-motion";
+import SignupForm from "components/auth/SignupForm";
 const Signup = () => {
-  const [role, setRole] = useState("Patient");
-  const [show1, setShow1] = useState(false)
-  const [show2, setShow2] = useState(false)
-  const handleClick1 = () => setShow1(!show1)
-  const handleClick2 = () => setShow2(!show2)
+  const roles = ["patient", "doctor", "hospital"];
+
+  const [role, setRole] = useState(roles[0]);
+
   const changeRole = (value) => {
-    setRole(value)
-  }
+    setRole(value);
+  };
+
   return (
-    <>
-      <div className={styles.signupMain}>
-        <Center className={cn(styles.left, styles[`left-${role}`])}>
-          <AnimatePresence>
-            {
-              role && (
-                <motion.img
-                  key={role}
-                  src={`/images/${role}.svg`}
-                  alt=""
-                  className={styles.signupImg}
-                  initial={{ y: 50, opacity: 0 }}
-                  animate={
-                    {
-                      y: 0,
-                      opacity: 1
-                    }
-                  }
-                  transition={{
-                    duration: 0.8,
-                    delay: 0.2,
-                    ease: easeOut
-                  }}
-                />
-              )
-            }
+    <div className={styles.signupMain}>
+      <Center className={cn(styles.left, styles[`left-${role}`])}>
+        <AnimatePresence>
+          {role && (
+            <motion.img
+              key={role}
+              src={`/images/${role}.svg`}
+              alt=""
+              className={styles.signupImg}
+              initial={{ y: 50, opacity: 0 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{
+                duration: 0.8,
+                delay: 0.2,
+                ease: easeOut,
+              }}
+            />
+          )}
+        </AnimatePresence>
+      </Center>
+      <div className={cn(styles.right, styles[`right-${role}`])}>
+        <div className={styles.logoContainer}>
+          <img src="/images/tempLogo.png" width={"5%"}></img>
+          <h1 className={styles.title}>MediBridge</h1>
+        </div>
 
-          </AnimatePresence>
-        </Center>
-        <div className={cn(styles.right, styles[`right-${role}`])}>
-          <div className={styles.logoContainer}>
-            <img src="/images/tempLogo.png" width={"5%"}></img>
-            <h1 className={styles.title}>MediBridge</h1>
-
+        <div className={styles.rightAll}>
+          <div className={styles.heading}>
+            <h2 className={styles[`title-${role}`]}>Signup</h2>
+            <p className={styles.p}>
+              We suggest using the email address you use at work.
+            </p>
           </div>
-          <Center className={styles.rightMain}>
-            <h2 className={styles.title}>Signup</h2>
-            <p className={styles.p}>We suggest using the email address you use at work.</p>
-            <FormControl className={styles.form}>
 
-              <InputGroup>
-                <InputLeftElement pointerEvents='none'>
-                  <AiOutlineMail />
-                </InputLeftElement>
-                <Input type='email' placeholder='Email' isRequired color={role === "Patient" ? "teal" : (role === "Doctor" ? "purple" : "orange")} _placeholder={{ color: 'inherit' }} className={styles.inp} />
-              </InputGroup>
-              <InputGroup>
-                <InputLeftElement>
-                  <AiFillLock />
-                </InputLeftElement>
-                <Input placeholder='Password' isRequired type={show1 ? 'text' : 'password'} color={role === "Patient" ? "teal" : (role === "Doctor" ? "purple" : "orange")} _placeholder={{ color: 'inherit' }} className={styles.inp} />
-                <InputRightElement onClick={() => { handleClick1() }}>
-                  {show1 ? <AiFillEye /> : <AiFillEyeInvisible />}
+          <Tabs
+            isFitted
+            variant="line"
+            className={styles.tabs}
+            onChange={(index) => {
+              changeRole(roles[index]);
+            }}
+          >
+            <TabList className={styles.tabList}>
+              <Tab className={cn(styles.tab, styles.patientTab)}>Patient</Tab>
+              <Tab className={cn(styles.tab, styles.doctorTab)}>Doctor</Tab>
+              <Tab className={cn(styles.tab, styles.hospitalTab)}>Hospital</Tab>
+            </TabList>
 
-                </InputRightElement>
-              </InputGroup>
-              <InputGroup>
-                <InputLeftElement>
-                  <AiFillLock />
-                </InputLeftElement>
-                <Input placeholder='Confirm Password' isRequired type={show2 ? 'text' : 'password'} color={role === "Patient" ? "teal" : (role === "Doctor" ? "purple" : "orange")} _placeholder={{ color: 'inherit' }} className={styles.inp} />
-                <InputRightElement onClick={() => { handleClick2() }}>
-                  {show2 ? <AiFillEye /> : <AiFillEyeInvisible />}
-                </InputRightElement>
-              </InputGroup>
-              <div className={cn(styles.selectionBox)} >
-                <p>Signup As</p>
-
-                <Select onChange={() => { changeRole(event.target.value) }} className={cn(styles.inp, styles.select)} color={role === "Patient" ? "teal" : (role === "Doctor" ? "purple" : "orange")}>
-                  <option value="Patient" color="teal">Patient</option>
-                  <option value="Doctor">Doctor</option>
-                  <option value="Hospital">Hospital</option>
-                </Select>
-              </div>
-            </FormControl>
-            <Button colorScheme={role === "Patient" ? "teal" : (role === "Doctor" ? "purple" : "orange")} variant='outline' className={styles.signupBtn}>Signup</Button>
-            <Center className={styles.googleContainer}>
-
-              <p className={styles.p}>Or</p>
-
-              <button className={styles.googleBtn}>
-                <img src="/images/google-logo.png" width={"40px"}></img>
-
-              </button>
-              {/* <button className={styles.googleBtn}>
-                <img src="/images/facebook-logo.png" width={"40px"}></img>
-
-              </button>
-              <button className={styles.googleBtn}>
-                <img src="/images/X-logo.png" width={"40px"}></img>
-
-              </button> */}
-              <p className={styles.p}>Already have an account? <Link className={styles.link} href='/auth/login'>Click here</Link></p>
-            </Center>
-
-          </Center>
+            <TabPanels>
+              <TabPanel>
+                <SignupForm role={role} />
+              </TabPanel>
+              <TabPanel>
+                <SignupForm role={role} />
+              </TabPanel>
+              <TabPanel>
+                <SignupForm role={role} />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
