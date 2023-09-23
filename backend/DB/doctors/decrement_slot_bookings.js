fc1bd8b5f546bc8fa68c89191567cc91b258ae3d
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2023-09-24 00:36:12 +0530

    appointment status

---
+++ backend/DB/doctors/decrement_slot_bookings.js
@@ -0,0 +1,16 @@
+const pool = require("../../config/connect_db");
+
+const decrement_slot_bookings = async (s_id) => {
+  const updateQuery =
+			"UPDATE doctor_slots SET booked_patients = booked_patients - 1 WHERE s_id = $1";
+	try {
+		const client = await pool.connect();
+		const result = await client.query(updateQuery, [s_id]);
+		client.release();
+		return result;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+module.exports = decrement_slot_bookings;
---
+++ backend/DB/patients/change_appointment_status.js
@@ -0,0 +1,20 @@
+const pool = require("../../config/connect_db");
+const decrement_slot_bookings = require("../doctors/decrement_slot_bookings");
+
+const change_appointment_status = async (status, a_id, s_id) => {
+	const query = `UPDATE appointments SET status = $1 WHERE a_id = $2;`;
+	const values = [status, a_id];
+	try {
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+  }
+  if (status == -1) {
+    await decrement_slot_bookings(s_id)
+  }
+};
+
+module.exports = change_appointment_status;
