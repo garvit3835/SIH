19dbe49bb4973a7d89530ca750f7eb28d3c52244
AnshGoyal27 <anshgoyal.cse@gmail.com>
2023-09-24 00:35:52 +0530

    Updated routes again

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -3,13 +3,15 @@ const { encrypt, checkCreds } = require('../utils/encrypt');
 const { assignToken, checkToken } = require('../utils/jwt');
 const insert_doctor_creds = require('../DB/doctors/insert_doctor_creds');
 const get_doctor_creds = require('../DB/doctors/get_doctor_creds');
+const get_doctor = require('../DB/doctors/get_doctor');
+const get_appointments = require('../DB/doctors/get_appointments');
 const router = express.Router();
 
 router.post('/login',async (req,res)=>{
     const {username,password} = req.body;
     const dbpass = await get_doctor_creds(username);
 
-    checkCreds(username,password).then((verify)=>{
+    checkCreds(username,password,dbpass).then((verify)=>{
         if(verify){
             assignToken(user,'4h').then((token)=>{
                 res.status(200).json({message:'Logged in Successfully',token:token})
@@ -23,13 +25,13 @@ router.post('/login',async (req,res)=>{
     })
 })
 
-router.post('/signup',(req,res)=>{
-    const {user,password} = req.body;
+router.post('/signup/creds',(req,res)=>{
+    const {email,password} = req.body;
 
     encrypt(password,15).then((pass)=>{
         console.log("Hashed: ",pass);
-        insert_doctor_creds(user,pass).then((p_id)=>{
-            assignToken(p_id,'4h').then((token)=>{
+        insert_patient_creds(email,pass).then((d_id)=>{
+            assignToken(d_id,'4h').then((token)=>{
                 res.status(201).json({message:'New Login Created Successfully',token:token})
             })
         }).catch((err)=>{
@@ -38,7 +40,30 @@ router.post('/signup',(req,res)=>{
     }).catch((err)=>{
         res.status(501).json({message:'Error Encountered'})
     })
+})
 
+router.post('/signup/info',checkToken,(req,res)=>{
+    const {name, number, latitude, longitude, specialization, rating, is_checkedin} = req.body;
+    const d_id = res.locals.authorization;
+
+})
+
+router.get('/details/all',checkToken,(req,res)=>{
+    const d_id = res.locals.authorization;
+    get_doctor(d_id).then((result)=>{
+        res.status(200).json({message:"Doctor details fetched",details:result});
+    }).catch((err)=>{
+        res.status(501).json({message:'Error Encountered'})
+    })
+})
+
+router.get('/details/appointments',checkToken,(req,res)=>{
+    const d_id = res.locals.authorization;
+    get_appointments(d_id).then((result)=>{
+        res.status(200).json({message:"Appointments fetched",details:result});
+    }).catch((err)=>{
+        res.status(501).json({message:'Error Encountered'})
+    })
 })
 
 router.post('/check',checkToken,(req,res)=>{
